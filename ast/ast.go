42cb3d36a13a69e0b4f18ce2ad2531c90e28598b
Allen <allendev@alphacode.co.kr>
2024-04-22 15:00:33 +0900

    String() 함수 및 테스트 추가

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "interpreter-go/token"
+import (
+	"bytes"
+	"interpreter-go/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // 표현식
@@ -27,9 +31,28 @@ func (program *Program) TokenLiteral() string {
 	if len(program.Statements) == 0 {
 		return ""
 	}
-
 	return program.Statements[0].TokenLiteral()
 }
+
+func (program *Program) String() string {
+	var out bytes.Buffer
+
+	for _, statement := range program.Statements {
+		out.WriteString(statement.String())
+	}
+
+	return out.String()
+}
+
+// Identifier : 식별자(=변수명) 토큰
+type Identifier struct {
+	Token token.Token // token.IDENT 토큰
+	Value string      // 변수명
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 // LetStatement : LET 표현식
 type LetStatement struct {
@@ -40,15 +63,22 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
 
-// Identifier : 식별자(=변수명) 토큰
-type Identifier struct {
-	Token token.Token // token.IDENT 토큰
-	Value string      // 변수명
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
-func (i *Identifier) expressionNode()      {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 // ReturnStatement : return 표현식
 type ReturnStatement struct {
 	Token       token.Token // token.RETURN 토큰
@@ -57,6 +87,20 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 // ExpressionStatement : 표현식문
 type ExpressionStatement struct {
 	Token      token.Token
@@ -65,3 +109,9 @@ type ExpressionStatement struct {
 
 func (es *ExpressionStatement) statementNode()       {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
